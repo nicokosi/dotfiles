aff8b1d4d453f28c35ec713f0ad38e573c546cff
Nicolas Kosinski <nicokosi@yahoo.com>
2015-12-18 04:53:11 +0100

    Aliases for showing Brew installed app versions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,3 @@
 alias reload!='. ~/.zshrc'
+alias brew-info-versions='for x in `brew list`; do brew info $x 2>/dev/null | head -n 1; done'
+alias brew-cask-info-versions='for x in `brew cask list`; do brew cask info $x 2>/dev/null | head -n 1; done'
