6299e6206a3ae1a238f9b9d9779a7890046d1b30
Nicolas Kosinski <nicokosi@yahoo.com>
2016-02-13 14:50:34 +0100

    Fix obsolete packages

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -15,15 +15,15 @@ fi
 # Install homebrew packages
 
 # "base":
-brew install brew-cask curl htop-osx lynx openssl rmtrash tree wget Caskroom/cask/xmind --with-cocoa --srgb emacs Caskroom/cask/gimp \
+brew install brew-cask curl lynx openssl rmtrash tree wget Caskroom/cask/xmind --with-cocoa --srgb emacs Caskroom/cask/gimp \
  Caskroom/cask/alfred Caskroom/cask/caffeine Caskroom/cask/chromium Caskroom/cask/iterm2 Caskroom/cask/spectacle Caskroom/cask/teamviewer \
- Caskroom/cask/cheatsheet Caskroom/cask/firefox-beta Caskroom/cask/openoffice Caskroom/cask/sublime-text3 Caskroom/cask/vlc-nightly Caskroom/cask/kobo Caskroom/cask/calibre
- Caskroom/cask/google-chrome Caskroom/cask/disk-inventory-x
+ Caskroom/cask/cheatsheet Caskroom/versions/firefox-beta Caskroom/cask/openoffice Caskroom/versions/sublime-text3 Caskroom/versions/vlc-nightly Caskroom/cask/kobo Caskroom/cask/calibre
+ Caskroom/cask/google-chrome Caskroom/cask/disk-inventory-x htop
 
 # virtual machines:
 brew install boot2docker docker Caskroom/cask/virtualbox Caskroom/cask/vagrant
 
 # development:
-brew install git Caskroom/cask/java Caskroom/cask/java7 Caskroom/cask/java6 jenv maven tig Caskroom/cask/intellij-idea-ce Caskroom/cask/sbt Caskroom/cask/lighttable leiningen typesafe-activator
+brew install git Caskroom/cask/java Caskroom/versions/java6 Caskroom/versions/java7 jenv maven tig Caskroom/versions/intellij-idea-ce sbt Caskroom/cask/lighttable leiningen typesafe-activator
 
 exit 0
