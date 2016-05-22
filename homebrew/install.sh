506dad9db6c93acd2fd5e189147aa96ff5fdda78
Nicolas Kosinski <nicokosi@yahoo.com>
2016-05-22 17:00:43 +0200

    Install postman

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -24,6 +24,5 @@ brew install brew-cask curl lynx openssl rmtrash tree wget Caskroom/cask/xmind -
 brew install boot2docker docker Caskroom/cask/virtualbox Caskroom/cask/vagrant
 
 # development:
-brew install git Caskroom/cask/java Caskroom/versions/java6 Caskroom/versions/java7 jenv maven tig Caskroom/versions/intellij-idea-ce sbt Caskroom/cask/lighttable leiningen typesafe-activator
-
+brew install git Caskroom/cask/java Caskroom/versions/java6 Caskroom/versions/java7 jenv maven tig Caskroom/versions/intellij-idea-ce sbt Caskroom/cask/lighttable leiningen typesafe-activator Caskroom/cask/postman
 exit 0
