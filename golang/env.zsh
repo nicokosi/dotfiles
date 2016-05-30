8141bd94a2335d24ce2accbe62eec97e2ea4484d
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2016-05-30 16:57:21 +0200

    Start to split env settings per app

---
+++ golang/env.zsh
@@ -0,0 +1,4 @@
+export GOPATH="~/work/gocode"
+export GOROOT="/opt/go"
+export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+
---
+++ maven/aliases.zsh
@@ -0,0 +1,7 @@
+alias mt="mvn test"
+alias mvnt=mt
+alias mci="mvn clean install"
+alias mvnci=mci
+alias mcist="mvn clean install -DskipTests"
+alias mvncist=mcist
+
