a84104dde4d3443648d9d89828daf21cc3d6381b
Nicolas Kosinski <nicokosi@yahoo.com>
2016-07-13 03:10:04 +0200

    Reformat homebrew install script
    One line per package.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -15,14 +15,55 @@ fi
 # Install homebrew packages
 
 # "base":
-brew install brew-cask curl lynx openssl rmtrash tree wget Caskroom/cask/xmind --with-cocoa --srgb emacs Caskroom/cask/gimp \
- Caskroom/cask/alfred Caskroom/cask/caffeine Caskroom/cask/chromium Caskroom/cask/iterm2 Caskroom/cask/spectacle Caskroom/cask/teamviewer \
- Caskroom/cask/cheatsheet Caskroom/versions/firefox-beta Caskroom/cask/openoffice Caskroom/versions/sublime-text3 Caskroom/versions/vlc-nightly Caskroom/cask/kobo Caskroom/cask/calibre
- Caskroom/cask/google-chrome Caskroom/cask/disk-inventory-x htop httpie
+brew install \
+  brew-cask \
+  curl \
+  lynx \
+  openssl \
+  rmtrash \
+  tree \
+  wget \
+  Caskroom/cask/xmind --with-cocoa --srgb \
+  emacs \
+  Caskroom/cask/gimp \
+  Caskroom/cask/alfred \
+  Caskroom/cask/caffeine \
+  Caskroom/cask/chromium \
+  Caskroom/cask/iterm2 \
+  Caskroom/cask/spectacle \
+  Caskroom/cask/teamviewer \
+  Caskroom/cask/cheatsheet \
+  Caskroom/versions/firefox-beta \
+  Caskroom/cask/openoffice \
+  Caskroom/versions/sublime-text3 \
+  Caskroom/versions/vlc-nightly \
+  Caskroom/cask/kobo \
+  Caskroom/cask/calibre \
+  Caskroom/cask/google-chrome \
+  Caskroom/cask/disk-inventory-x \
+  htop \
+  httpie
 
 # virtual machines:
-brew install boot2docker docker Caskroom/cask/virtualbox Caskroom/cask/vagrant
+brew install \
+  docker \
+  Caskroom/cask/virtualbox \
+  Caskroom/cask/vagrant
 
 # development:
-brew install git Caskroom/cask/java Caskroom/versions/java6 Caskroom/versions/java7 jenv maven tig Caskroom/versions/intellij-idea-ce sbt Caskroom/cask/lighttable leiningen typesafe-activator Caskroom/cask/postman
+brew install \
+  git \
+  Caskroom/cask/java \
+  Caskroom/versions/java6 \
+  Caskroom/versions/java7 \
+  jenv \
+  maven \
+  tig \
+  Caskroom/versions/intellij-idea-ce \
+  sbt \
+  Caskroom/cask/lighttable \
+  leiningen \
+  typesafe-activator \
+  Caskroom/cask/postman
+
 exit 0
