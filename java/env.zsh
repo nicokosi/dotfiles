e3255085a6cb673b24995918d126d71ebcdfda0a
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2016-08-01 13:45:49 +0200

    Move jenv conf to java module

--- java/env.zsh
+++ java/env.zsh
@@ -1,1 +1,7 @@
 export JAVA_HOME="/opt/jdk1.8.0_92"
+
+# jEnv configuration.
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+
+jenv enable-plugin maven > /dev/null 2>&1
--- system/env.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# jEnv configuration.
-export PATH="$HOME/.jenv/bin:$PATH"
-eval "$(jenv init -)"
-
-jenv enable-plugin maven > /dev/null 2>&1
