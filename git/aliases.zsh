4648664d08b17c8028d58cd64a7c80818c68e893
Nicolas Kosinski <nicokosi@yahoo.com>
2016-08-05 06:12:15 +0200

    Add git aliases for 'add --patch' and 'reset --hard'

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,5 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gap='git add -p'
+alias grh='git reset --hard @{u}'
