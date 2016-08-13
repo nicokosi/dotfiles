93bfa031cd8bc6d06abde62549e1d47d46303b1b
Nicolas Kosinski <nicokosi@yahoo.com>
2016-08-13 19:13:03 +0200

    Install Pelican static site generator
    Also add aliases that fix Python encoding/charset error.

---
+++ pelican/aliases.zsh
@@ -0,0 +1,4 @@
+alias pelican="LC_ALL=\"en_US.UTF-8\" LANG=\"en_US.UTF-8\" pelican"
+alias pelican-quickstart="LC_ALL=\"en_US.UTF-8\" LANG=\"en_US.UTF-8\" pelican-quickstart"
+alias pelican-make-html="LC_ALL=\"en_US.UTF-8\" LANG=\"en_US.UTF-8\" make html"
+alias pelican-make-serve="LC_ALL=\"en_US.UTF-8\" LANG=\"en_US.UTF-8\" make serve"
---
+++ pelican/install.sh
@@ -0,0 +1,8 @@
+brew install python
+pip install pelican
+pip install markdown
+
+virtualenv ~/virtualenvs/pelican
+cd ~/virtualenvs/pelican
+. bin/activate
+
