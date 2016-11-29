6ed35cf879b88c48d7974077f7af04bf6399fa44
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2016-11-29 08:50:29 +0100

    Install jessfraz/audit for listing protected GitHub branches

--- git/install.sh
+++ git/install.sh
@@ -1,2 +1,7 @@
+# Command line tool that enhances 'git diff':
 apt-get install npm
 npm install -g diff-so-fancy
+
+# Command line tool that audits all GitHub repositories:
+go install github.com/jessfraz/audit
+
