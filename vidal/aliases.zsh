bc8ffa3858afe790fcd71450ce1923dd5963c752
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2016-12-19 17:34:01 +0100

    Add aliases for git merges after Vidal releases

--- vidal/aliases.zsh
+++ vidal/aliases.zsh
@@ -1,3 +1,5 @@
 alias dockersw="DOCKER_HOST=\"tcp://dockersw.vidal.net:4243\" docker"
 alias mvn-deps-tree-vidal="mvn dependency:tree -Dincludes=\"com.vidal*:*:*:*\""
 alias mvn-version="xmlstarlet sel -N ns=\"http://maven.apache.org/POM/4.0.0\" -t -c \"/ns:project/ns:version/text()\" -n pom.xml"
+alias check-merges=~/.dotfiles/vidal/check-merges.sh
+alias git-multi-fetch=~/.dotfiles/vidal/git-multi-fetch.sh
