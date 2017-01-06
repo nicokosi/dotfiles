fa7ac6625efe93c5dacd102fe8e736bd4f1e2818
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2017-01-06 08:34:42 +0100

    Add Vagrant aliases

--- vagrant/aliases.zsh
+++ vagrant/aliases.zsh
@@ -1,1 +1,12 @@
 alias v=vagrant
+alias vup="vagrant up"
+alias vst="vagrant status"
+alias vh="vagrant halt"
+alias vd="vagrant destroy"
+alias vdf="vagrant destroy --force"
+alias vssh="vagrant ssh"
+
+alias vsl="vagrant snapshot list"
+alias vss="vagrant snapshot save"
+alias vsr="vagrant snapshot restore"
+alias vsd="vagrant snapshot delete"
