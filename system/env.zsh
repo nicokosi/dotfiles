c3c6659a7a62ab6849c9e9d3c66ad01a376fc59c
Nicolas Kosinski <nicokosi@yahoo.com>
2017-02-08 19:17:58 +0100

    Show hidden files in Finder

--- system/env.zsh
+++ system/env.zsh
@@ -11,5 +11,8 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
+# Show hidden files in Finder
+defaults write com.apple.finder AppleShowAllFiles 1
+
 # Do not prompt for SSH keys
 ssh-add -A 2>/dev/null
