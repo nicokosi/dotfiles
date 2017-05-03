b1e4f1a552ac9af46340ec4925b6a89cfc2e13a6
Nicolas Kosinski <nicokosi@yahoo.com>
2017-05-03 20:55:42 +0200

    Customize macOS' Finder and iTerm preferences

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,8 +1,27 @@
-# Sets reasonable OS X defaults.
-#
-# Or, in other words, set shit how I like in OS X.
-#
-# The original idea (and a couple settings) were grabbed from:
-#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
-#
-# Run ./set-defaults.sh and you'll be good to go.
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+
+###############################################################################
+# iTerm 2                                                                     #
+###############################################################################
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
