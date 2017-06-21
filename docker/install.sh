82236c41047dc21ee9fc45023b6f211bbbb41408
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2017-06-21 08:41:56 +0200

    Enhance docker installation script
    Run steps documented in:
    https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository
    
    Use stable version, explicitly.

--- docker/install.sh
+++ docker/install.sh
@@ -1,3 +1,23 @@
-sudo apt-cache update
-sudo apt-get install docker-ce
-sudo apt-get install docker-compose
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
+echo "${GREEN}Install packages to allow apt to use a repository over HTTPS${NO_COLOR}:"
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+echo "${GREEN}Add Docker’s official GPG key${NO_COLOR}:"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo "${GREEN}Set up the stable repository${NO_COLOR}:"
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+
+echo "${GREEN}Install docker-ce and docker-compose${NO_COLOR}:"
+apt-get install docker-ce
+apt-get install docker-compose
