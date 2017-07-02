cd7c3715fe894d67e94bda062b8bf6dd1ba47081
Nicolas Kosinski <nicokosi@yahoo.com>
2017-07-02 20:03:29 +0200

    Use long options for clamscan antivirus

--- clamav/aliases.zsh
+++ clamav/aliases.zsh
@@ -1,1 +1,1 @@
-alias antivirus-check='freshclam ; for folder in `find / -maxdepth 1 -type d -print | grep -v "^/$"` ; do clamscan -r --bell -i -r $folder ; echo "Analyzed folder $folder" ; done'
+alias antivirus-check='freshclam ; for folder in `find / -maxdepth 1 -type d -print | grep -v "^/$"` ; do clamscan --bell --infected --recursive $folder ; echo "Analyzed folder $folder" ; done'
