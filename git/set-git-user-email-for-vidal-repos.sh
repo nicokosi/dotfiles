194f85e745541370b9ef62381a0b23264e123439
Nicolas Kosinski <nicokosi@yahoo.com>
2017-07-04 06:40:28 +0200

    Enhance script that adds git user.email for Vidal repos
    Filter git folders and output user.email

--- git/set-git-user-email-for-vidal-repos.sh
+++ git/set-git-user-email-for-vidal-repos.sh
@@ -1,8 +1,13 @@
 echo "Set git user.email to \"nicolas.kosinski@vidal.fr\" for all repos in ~/work/vidal"
 vidal_folder="/Users/nicolas/work/vidal"
 vidal_repos=`find $vidal_folder -maxdepth 1 -type d -print | grep --invert-match "\.$"`
-for folder in $vidal_repos ; do cd $folder ; echo $folder ; git config user.email "nicolas.kosinski@vidal.fr" ; done
+for folder in $vidal_repos
+do
+  if [[ ( -d $folder && -d $folder/.git ) ]]; then
+    cd $folder
+    echo "\n$folder"
+    git config user.email "nicolas.kosinski@vidal.fr"
+    git config --local --get user.email
+  fi
+done
 
-echo "Done!\n"
-echo "Check:"
-for folder in $vidal_repos ; do cd $folder ; echo $folder ; git config --local --get user.email ; done
