1ff699e2f8149649ecf6a8e1e824286f76664a43
Nicolas Kosinski <nicokosi@yahoo.com>
2017-07-08 12:47:47 +0200

    Auto upgrade brew casks and cleanup brew

--- brew/aliases.zsh
+++ brew/aliases.zsh
@@ -1,1 +1,1 @@
-alias brew-upgrades="brew update && brew upgrade && mas upgrade && brew cask outdated"
+alias brew-upgrades="brew update && brew upgrade --cleanup && mas upgrade && for outdated in `brew cask outdated --quiet` ; brew cask reinstall $outdated && brew cleanup && brew cask cleanup"
