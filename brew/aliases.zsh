f36c2741ae973282291b131342c481bf4ecf13a0
Nicolas Kosinski <nicokosi@yahoo.com>
2017-07-15 18:05:09 +0200

    Auto upgrade brew casks and cleanup brew (cont.)
    Fix for loop.

--- brew/aliases.zsh
+++ brew/aliases.zsh
@@ -1,1 +1,1 @@
-alias brew-upgrades="brew update && brew upgrade --cleanup && mas upgrade && for outdated in `brew cask outdated --quiet` ; brew cask reinstall $outdated && brew cleanup && brew cask cleanup"
+alias brew-upgrades="brew update && brew upgrade --cleanup && mas upgrade && for outdated in `brew cask outdated --quiet` ; do ; brew cask reinstall $outdated && brew cleanup && brew cask cleanup ; done"
