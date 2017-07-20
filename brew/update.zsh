5e3b9e4db277e7d59b2e3dbe65490812e6665c46
Nicolas Kosinski <nicokosi@yahoo.com>
2017-07-21 00:50:25 +0200

    Add function for updating both shell and apps
    Function replaces previous alias that did not work.
    Also adds colors! 🌈

--- brew/update.zsh
+++ brew/update.zsh
@@ -1,1 +1,30 @@
-alias brew-upgrades="brew update && brew upgrade --cleanup && mas upgrade && for outdated in `brew cask outdated --quiet` ; do ; brew cask reinstall $outdated && brew cleanup && brew cask cleanup ; done"
+echo-in() { #args= color, text
+  local NO_COLOR="\033[0m"
+  echo "${1}${2}${NO_COLOR}"
+}
+echo-in-blue() { echo-in "\033[0;34m" $1}
+echo-in-green() { echo-in "\033[0;92m" $1}
+
+brew-upgrades() {
+  echo-in-blue "Upgrading brew"
+  brew update
+  brew upgrade --cleanup
+
+  echo-in-blue "Upgrading mas"
+  mas upgrade
+
+  echo-in-blue "Upgrading brew cask"
+  for outdated in "${(@f)$(brew cask outdated --quiet)}"
+  do
+    if [[ !  -z  $outdated  ]]
+    then
+      brew cask reinstall $outdated
+    fi
+  done
+
+  echo-in-blue "Cleanup"
+  brew cleanup
+  brew cask cleanup
+
+  echo-in-green "Done!"
+}
