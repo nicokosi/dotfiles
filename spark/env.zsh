666110da804d6e315fcb13d0ea72f198bc20b910
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2017-08-07 09:50:00 +0200

    Install Apache Spark 2.2

---
+++ spark/env.zsh
@@ -0,0 +1,1 @@
+export PATH="$PATH:/opt/spark-2.2.0-bin-hadoop2.7/bin/"
---
+++ spark/install.sh
@@ -0,0 +1,6 @@
+# See https://spark.apache.org/downloads.html
+wget -P /tmp https://d3kbcqa49mib13.cloudfront.net/spark-2.2.0-bin-hadoop2.7.tgz
+
+cd /tmp
+tar -xzvf spark-2.2.0-bin-hadoop2.7.tgz
+sudo mv spark-2.2.0-bin-hadoop2.7 /opt
