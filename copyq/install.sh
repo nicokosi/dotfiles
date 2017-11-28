dd97a8086f020b8120fbe78f5f4b82faf214b060
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2017-11-28 09:27:41 +0100

    Launch copyq at startup

--- copyq/install.sh
+++ copyq/install.sh
@@ -2,3 +2,5 @@ add-apt-repository ppa:hluk/copyq
 apt update
 apt install copyq
 
+cp /usr/share/applications/copyq.desktop $HOME/.config/autostart/ && chmod +x $HOME/.config/autostart/copyq.desktop
+
