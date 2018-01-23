a7a2b6da3b31dbd428c80b9a088a8ff9a6bb9f65
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-01-23 17:25:50 +0100

    Add Docker / ctop aliases for dockerbo.vidal.net

--- vidal/aliases.zsh
+++ vidal/aliases.zsh
@@ -1,5 +1,9 @@
 alias dockersw="DOCKER_HOST=\"tcp://dockersw.vidal.net:4243\" docker"
+alias dockerbo="DOCKER_HOST=\"tcp://dockerbo.vidal.net:4243\" docker"
+
 alias ctop-dockersw="DOCKER_HOST=\"tcp://dockersw.vidal.net:4243\" ctop -a"
+alias ctop-dockerbo="DOCKER_HOST=\"tcp://dockerbo.vidal.net:4243\" ctop -a"
+
 alias mvn-deps-tree-vidal="mvn dependency:tree -Dincludes=\"com.vidal*:*:*:*\""
 alias mvn-version="xmlstarlet sel -N ns=\"http://maven.apache.org/POM/4.0.0\" -t -c \"/ns:project/ns:version/text()\" -n pom.xml"
 alias check-merges=~/.dotfiles/vidal/check-merges.sh
