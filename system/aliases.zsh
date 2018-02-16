nicokosi/dotfiles

commit 7a33c7b393e10dd2b0469a730c771e2049ade5de

Do not automatically upgrade distribution
It is wiser to upgrade distribution on demand.
And use the cache, Luke! :)

system/aliases.zsh
@@ -11,4 +11,4 @@ fi
 
 alias open="xdg-open"
 
-alias apt-all-upgrades="sudo apt update && sudo apt upgrade && sudo apt-get dist-upgrade && sudo apt-get autoclean"
+alias apt-all-upgrades="sudo apt update && sudo apt upgrade && && sudo apt-get autoremove"

commit ed5aa12d78be7b2f9d2ad83f11ea622f65fb8a98

Fix 'apt-all-upgrades' alias broken in commit 7a33c7b393e10dd2b0469a730c771e2049ade5de

system/aliases.zsh
@@ -11,4 +11,4 @@ fi
 
 alias open="xdg-open"
 
-alias apt-all-upgrades="sudo apt update && sudo apt upgrade && && sudo apt-get autoremove"
+alias apt-all-upgrades="sudo apt update && sudo apt upgrade && sudo apt-get autoremove"