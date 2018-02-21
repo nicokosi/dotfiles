be012e79729ed1732bfd5fe057ec31e4e390dd70
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-02-21 13:53:52 +0100

    Instal exa, "a modern replacement for 'ls'" 📂

---
+++ exa/install.sh
@@ -0,0 +1,6 @@
+cd /tmp
+wget https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip
+unzip exa-linux-x86_64-0.8.0.zip
+mv exa-linux-x86_64 exa
+mv exa /usr/local/bin/exa
+chmod u+x /usr/local/bin/exa
