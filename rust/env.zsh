c61cac0c34510b27749f493225a6487d1162a70b
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-04-16 09:00:31 +0200

    Install 'rusti' Rust REPL
    https://github.com/murarth/rusti

--- rust/env.zsh
+++ rust/env.zsh
@@ -1,1 +1,4 @@
 export PATH=$HOME/.cargo/bin:$PATH
+
+alias rust-repl="rustup run nightly-2016-08-01 ~/.cargo/bin/rusti"
+alias rusti=rust-repl
--- rust/install.sh
+++ rust/install.sh
@@ -5,3 +5,9 @@ rustup update
 
 # Install rustfmt
 rustup component add rustfmt-preview
+
+# Install rusti
+apt install libncursesw5-dev
+rustup install nightly-2016-08-01
+rustup run nightly-2016-08-01 cargo install --git https://github.com/murarth/rusti
+
