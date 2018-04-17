61b2dadaf34e4db894f27e58b90ed3e800801cec
Nicolas Kosinski <nicokosi@yahoo.com>
2018-04-17 07:08:32 +0200

    Install 'rustfmt' Rust code formatter
    https://github.com/rust-lang-nursery/rustfmt

--- rust/install.sh
+++ rust/install.sh
@@ -4,3 +4,6 @@ rustup-init -y
 # Install 'rusti' Rust REPL
 rustup run nightly-2016-08-01
 rustup run nightly-2016-08-01 cargo install --git https://github.com/murarth/rusti
+
+# Install 'rustfmt' Rust code formatter
+rustup component add rustfmt-preview
