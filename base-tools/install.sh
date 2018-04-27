25821500283dda43224fd6f6955d3401aff34611
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-04-27 08:17:07 +0200

    Install Reflex "to watch a directory and rerun a command when certain files change"
    https://github.com/cespare/reflex

--- base-tools/install.sh
+++ base-tools/install.sh
@@ -18,3 +18,6 @@ apt install \
 
 wget https://github.com/bcicen/ctop/releases/download/v0.6.1/ctop-0.6.1-linux-amd64 -O /usr/local/bin/ctop
 chmod +x /usr/local/bin/ctop
+
+go get github.com/cespare/reflex
+go install github.com/cespare/reflex
