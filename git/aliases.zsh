2fd259d1857b389092ea973a342e0e253c06fdee
Nicolas Kosinski <nicokosi@yahoo.com>
2018-05-08 18:23:15 +0200

    Add 'gpf' alias for "git push --force-with-lease"

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -22,6 +22,7 @@ alias gl='git pull --prune'
 alias glo='git log --oneline --decorate --color'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gpf='git push --force-with-lease'
 alias grh='git reset --hard @{u}'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gst='git status'
