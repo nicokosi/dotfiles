8b4c23b8a87c9f63a81587c9bed9a8b98c397cc9
Nicolas Kosinski <nicokosi@yahoo.com>
2018-06-21 06:53:38 +0200

    Install clippy, 'a bunch of lints to catch common mistakes and improve your Rust code'

--- rust/install.sh
+++ rust/install.sh
@@ -7,3 +7,7 @@ rustup run nightly-2016-08-01 cargo install --git https://github.com/murarth/rus
 
 # Install 'rustfmt' Rust code formatter
 rustup component add rustfmt-preview
+
+# Install clippy, 'a bunch of lints to catch common mistakes and improve your Rust code'
+rustup update nightly
+rustup run nightly cargo install --force clippy
