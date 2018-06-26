bdcc68b95b2ca468fbb0652f13c3f63cbb29b5b6
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-06-26 08:27:40 +0200

    Fix Rust's Clippy installation/configuration 🦀
    Copied/pasted from https://github.com/nicokosi/dotfiles/blob/master/rust 😇

--- rust/env.zsh
+++ rust/env.zsh
@@ -2,3 +2,6 @@ export PATH=$HOME/.cargo/bin:$PATH
 
 alias rust-repl="rustup run nightly-2016-08-01 ~/.cargo/bin/rusti"
 alias rusti=rust-repl
+
+alias clippy='rustup run nightly cargo clippy'
+alias rust-lint=clippy
--- rust/install.sh
+++ rust/install.sh
@@ -11,3 +11,6 @@ apt install libncursesw5-dev
 rustup install nightly-2016-08-01
 rustup run nightly-2016-08-01 cargo install --git https://github.com/murarth/rusti
 
+# Install clippy, 'a bunch of lints to catch common mistakes and improve your Rust code'
+rustup update nightly
+rustup run nightly cargo install --force clippy
