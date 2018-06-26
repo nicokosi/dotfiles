b81739abed6dea4cf2ab618650170524d898c863
Nicolas Kosinski <nicokosi@yahoo.com>
2018-06-26 07:16:15 +0200

    Enhance Rust Clippy alias: do not rebuild all for lint warnings

--- rust/env.zsh
+++ rust/env.zsh
@@ -1,5 +1,5 @@
 alias rust-repl='rustup run nightly-2016-08-01 ~/.cargo/bin/rusti'
 alias rusti=rust-repl
 
-alias clippy='cargo clean && rustup run nightly cargo clippy'
+alias clippy='rustup run nightly cargo clippy'
 alias rust-lint=clippy
