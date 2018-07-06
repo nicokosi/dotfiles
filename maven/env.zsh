dffc908851d55756c1c7a574082255849a54465e
Nicolas Kosinski <nicokosi@yahoo.com>
2018-07-06 07:25:10 +0200

    Add a Maven alias to update all dependencies to latest patch

--- maven/env.zsh
+++ maven/env.zsh
@@ -1,1 +1,2 @@
 alias mvn-vidal='mvn --settings ~/.m2/vidal-settings.xml'
+alias mvn-update-deps-to-latest-patch='mvn versions:use-latest-versions -DallowAnyUpdates=false -DallowMajorUpdates=false -DallowMinorUpdates=false -DallowIncrementalUpdates=true -DallowSnapshots=false'
