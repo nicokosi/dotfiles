5b4345028ef3a39a7cfc7fa163f91338af02f6d4
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-07-06 08:22:03 +0200

    Add a Maven alias to update all dependencies to latest patch

--- maven/aliases.zsh
+++ maven/aliases.zsh
@@ -5,3 +5,4 @@ alias mvnci=mci
 alias mcist="mvn clean install -DskipTests"
 alias mvncist=mcist
 alias mvn-jacoco='mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify org.jacoco:jacoco-maven-plugin:report'
+alias mvn-update-deps-to-latest-patch='mvn versions:use-latest-versions -DallowAnyUpdates=false -DallowMajorUpdates=false -DallowMinorUpdates=false -DallowIncrementalUpdates=true -DallowSnapshots=false'
