b644a17bc1b1e33af81f7b9acdf94c0d31a36638
Nicolas Kosinski <nicokosi@yahoo.com>
2018-08-14 12:00:37 +0200

    Install cespare/reflex to "Run a command when files change"

--- dev-tools/install.sh
+++ dev-tools/install.sh
@@ -1,4 +1,6 @@
-# Install boyter/scc, "a very fast accurate code counter with complexity calculations" 
+# Install boyter/scc, "a very fast accurate code counter with complexity calculations"
 go install github.com/boyter/scc
 mv $GOPATH/bin/scc /usr/local/bin/
 
+# Install cespare/reflex to "Run a command when files change"
+go get github.com/cespare/reflex
