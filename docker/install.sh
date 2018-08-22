06bbfb3c8b53291efd0d7209a4419fda86e67113
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-08-22 17:21:12 +0200

    Configure Docker: set DNS and bridge network IP

--- docker/install.sh
+++ docker/install.sh
@@ -29,5 +29,11 @@ curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersio
 # Fix "must run as sudo" issue:
 groupadd docker
 gpasswd -a ${USER} docker
-service docker restart
 
+# Configure 'bridge' network IP and DNS for Vidal
+echo "{
+  \"bip\": \"10.10.10.1/24\",
+  \"dns\": [\"172.16.50.4\", \"8.8.8.8\"]
+}" > /etc/docker/daemon.json
+
+service docker restart
