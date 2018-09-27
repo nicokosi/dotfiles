6bf0f1e4697b1bef317b19a130bb6f415c0a10b8
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-09-27 08:37:59 +0200

    Install travis CLI tool
    Usage example:
    
      $ git config --get remote.origin.url
      git@github.com:nicokosi/java-for-katas.git
      $ travis status
      build #36 passed
    
    Project documentation: https://github.com/travis-ci/travis.rb

---
+++ travis/env.zsh
@@ -0,0 +1,3 @@
+# added by travis gem
+[ -f /home/nkosinski/.travis/travis.sh ] && source /home/nkosinski/.travis/travis.sh
+
---
+++ travis/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gem install travis -v 1.8.9 --no-rdoc --no-ri
