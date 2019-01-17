nicokosi/dotfiles

commit 64f184c6f8ec7ec3bd5984cb6f3c6a85f1cc20dd

Remove homebrew warning 'brew upgrade --cleanup' is deprecated!'
Remove this warning with homebrew 1.9:

  Warning: Calling 'brew upgrade --cleanup' is deprecated! Use 'HOMEBREW_INSTALL_CLEANUP' instead.

See https://brew.sh/2019/01/09/homebrew-1.9.0/:
  brew upgrade --cleanup and HOMEBREW_UPGRADE_CLEANUP have been replaced with the HOMEBREW_INSTALL_CLEANUP variable. These will become default behaviours in 2.0.0.

brew/update.zsh
@@ -6,8 +6,7 @@ echo-in-blue() { echo-in "\033[0;34m" $1}
 
 brew-upgrades() {
   echo-in-blue "Upgrading brew"
-  brew update
-  brew upgrade --cleanup
+  HOMEBREW_INSTALL_CLEANUP=true brew update
 
   echo-in-blue "Upgrading mas"
   mas upgrade

commit a68669d83baabebbf8d43ee647d2150e23e06f82

Make 'brew-updates' *really* upgrade brews ✅
'brew-updates' has been broken in commit 64f184c.
It can now upgrade brews again.

brew/env.zsh
@@ -1,0 +1,4 @@
+#!/bin/sh
+
+export HOMEBREW_INSTALL_CLEANUP=true
+

brew/update.zsh
@@ -6,7 +6,8 @@ echo-in-blue() { echo-in "\033[0;34m" $1}
 
 brew-upgrades() {
   echo-in-blue "Upgrading brew"
-  HOMEBREW_INSTALL_CLEANUP=true brew update
+  brew update
+  brew upgrade
 
   echo-in-blue "Upgrading mas"
   mas upgrade