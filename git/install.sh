45576704965bc01bcbec4e4e818c4b49e113bff0
Nicolas Kosinski <nicokosi@yahoo.com>
2019-06-04 07:16:19 +0200

    Install gitmoji-cli to add emojis in git commits ✨
    https://github.com/carloscuesta/gitmoji-cli

--- git/install.sh
+++ git/install.sh
@@ -4,3 +4,6 @@ brew install diff-so-fancy
 
 # Command line tool for launching multiple git commands in parallel
 go install github.com/jcgay/parallel-git-repo
+
+# Command line tool for using emojis in commits
+npm i -g gitmoji-cli
