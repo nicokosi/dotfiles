4148d408b607587af779dcde560e0846b075916e
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2019-07-19 09:44:38 +0200

    🎨 Install Rust via Homebrew

--- rust/env.zsh
+++
@@ -1,7 +0,0 @@
-export PATH=$HOME/.cargo/bin:$PATH
-
-alias rust-repl="rustup run nightly-2016-08-01 ~/.cargo/bin/rusti"
-alias rusti=rust-repl
-
-alias clippy='rustup run nightly cargo clippy'
-alias rust-lint=clippy
