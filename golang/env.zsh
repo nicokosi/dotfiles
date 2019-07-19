67174dc684a03e5529c397eb98aa021e6a89bd9c
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2019-07-19 09:43:04 +0200

    🎨 Install Golang via Homebrew

--- golang/env.zsh
+++ golang/env.zsh
@@ -1,3 +1,2 @@
 export GOPATH="/home/nkosinski/work/gocode"
-export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
 
--- golang/install.sh
+++
@@ -1,4 +0,0 @@
-add-apt-repository ppa:longsleep/golang-backports
-
-apt install golang-go
-mkdir -p /home/nkosinski/work/gocode
