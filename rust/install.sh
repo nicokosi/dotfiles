0ccc881c37cafe4f6671e2868180ce77eefff2de
Nicolas Kosinski <nicokosi@yahoo.com>
2019-07-31 06:02:07 +0200

    ♻️ Make 'clippy' (Rust linter) install up-to-date

--- rust/install.sh
+++ rust/install.sh
@@ -6,4 +6,4 @@ rustup component add rustfmt
 
 # Install clippy, 'a bunch of lints to catch common mistakes and improve your Rust code'
 rustup update
-rustup component add clippy-preview
+rustup component add clippy
