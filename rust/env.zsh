3d4a935045316668366f78327ba0b851141932fa
Nicolas Kosinski <nicokosi@yahoo.com>
2019-07-31 06:02:07 +0200

    🔥 Uninstall rusti (Rust REPL)
    1. It is deprecrated:
    
      The rusti project is deprecated. It is not recommended for regular use.
    
    2. It does not work anymore on my machine.

--- rust/env.zsh
+++ rust/env.zsh
@@ -1,4 +1,1 @@
-alias rust-repl='rustup run nightly-2016-08-01 ~/.cargo/bin/rusti'
-alias rusti=rust-repl
-
 alias rust-lint='cargo clippy'
--- rust/install.sh
+++ rust/install.sh
@@ -1,10 +1,6 @@
 # (Re)install rustup
 rustup-init -y
 
-# Install 'rusti' Rust REPL
-rustup run nightly-2016-08-01
-rustup run nightly-2016-08-01 cargo install --git https://github.com/murarth/rusti
-
 # Install 'rustfmt' Rust code formatter
 rustup component add rustfmt-preview
 
