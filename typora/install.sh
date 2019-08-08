52b82c6153dddd9d65d2bafe8f4692676a003fd5
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2019-08-08 08:33:19 +0200

    ✨ Install Typora, "a minimal Markdown editor"
    https://typora.io/

---
+++ typora/install.sh
@@ -0,0 +1,12 @@
+# or run:
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+
+# add Typora's repository
+add-apt-repository 'deb https://typora.io/linux ./'
+
+apt-get update
+
+# install typora
+apt-get install typora
