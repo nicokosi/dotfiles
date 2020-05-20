33b4b621d6001219f5f6437972f8e8fcffbc5e03
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2020-05-20 07:24:47 +0200

    Make rupa/z work again! (Jump around! Jump! Jump!)

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -1,20 +1,23 @@
 # Antigen
 source "$HOME/antigen.zsh"
-# Load the oh-my-zsh's library.
+
 antigen use oh-my-zsh
-# Plugins
+
 antigen bundles <<EOBUNDLES
-docker
-mvn
-gitignore
-history
-vagrant
-rupa/z
 arialdomartini/oh-my-git
-zsh-users/zsh-syntax-highlighting
+docker
+# git
+gitignore
 gradle
+history
+mvn
+rupa/z
+vagrant
+zsh-users/zsh-syntax-highlighting
 EOBUNDLES
 
 antigen theme arialdomartini/oh-my-git-themes oppa-lana-style
 
 antigen apply
+
+source $HOME/.antigen/bundles/rupa/z/z.sh
