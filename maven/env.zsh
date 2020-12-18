d624adcd68918a8fe6c62f8f25bed06d79531c26
Nicolas Kosinski <nicokosi@yahoo.com>
2020-12-18 06:55:17 +0100

    ➖ Remove 'mvnd' Maven alias
    It conflicts with the Maven Daemon (https://github.com/mvndaemon/mvnd).

--- maven/env.zsh
+++ maven/env.zsh
@@ -1,2 +1,5 @@
 alias mvn-vidal='mvn --settings ~/.m2/vidal-settings.xml'
 alias mvn-update-deps-to-latest-patch='mvn versions:use-latest-versions -DallowAnyUpdates=false -DallowMajorUpdates=false -DallowMinorUpdates=false -DallowIncrementalUpdates=true -DallowSnapshots=false'
+
+# Unalias 'mvnd' which conflicts with the Maven Daemon (https://github.com/mvndaemon/mvnd)
+unalias mvnd
