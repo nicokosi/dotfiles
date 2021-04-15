c14db86658135ebbc7892037608d22962af951da
Nicolas Kosinski <nicokosi@yahoo.com>
2021-04-15 08:01:26 +0200

    Define Sublime Text as shell's default text editor

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -40,3 +40,6 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+export EDITOR=/usr/local/bin/subl
+export VISUAL=/usr/local/bin/subl
