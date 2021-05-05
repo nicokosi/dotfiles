e8d89937cce77df66e274c2f599fc5d4c14647aa
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2021-05-05 09:20:26 +0200

    Fix "go install" in git/install.sh
    Error, before fix:
    
    	go install: version is required when current directory is not in a module

--- git/install.sh
+++ git/install.sh
@@ -1,8 +1,8 @@
 # Command line tool that audits all GitHub repositories:
-go install github.com/jessfraz/audit
+go install 'github.com/jessfraz/audit@latest'
 
 # Command line tool that protects branches of all GitHub repositories:
-go install github.com/jcgay/protector
+go install 'github.com/jcgay/protector@latest'
 
 # Command line tool for launching multiple git commands in parallel
-go install github.com/jcgay/parallel-git-repo
+go install 'github.com/jcgay/parallel-git-repo@latest'
