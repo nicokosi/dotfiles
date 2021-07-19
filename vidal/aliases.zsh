03a945527e1322639d197bba14a1b3a48b09b914
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2021-07-19 08:34:27 +0200

    Fix vpn Vidal alias: launch in background

--- vidal/aliases.zsh
+++ vidal/aliases.zsh
@@ -10,4 +10,5 @@ alias check-merges=~/.dotfiles/vidal/check-merges.sh
 alias git-multi-fetch=~/.dotfiles/vidal/git-multi-fetch.sh
 alias git-multi-tag-list=~/.dotfiles/vidal/git-multi-tag-list.sh
 
-alias vpn='cd /opt/fortisslvpn/forticlientsslvpn && ./fortisslvpn.sh &> /dev/null'
+alias vpn='cd /opt/fortisslvpn/forticlientsslvpn && ./fortisslvpn.sh &> /dev/null &'
+alias gap="git add --patch"
