744dbcb62f1003a68f56f4d524551354e1876039
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2021-10-14 09:59:52 +0200

    Configure Docker alias for dockersw2.vidal.net

--- vidal/aliases.zsh
+++ vidal/aliases.zsh
@@ -4,6 +4,8 @@ alias dockerbo="DOCKER_HOST=\"tcp://dockerbo.vidal.net:4243\" docker"
 alias ctop-dockersw="DOCKER_HOST=\"tcp://dockersw.vidal.net:4243\" ctop -a"
 alias ctop-dockerbo="DOCKER_HOST=\"tcp://dockerbo.vidal.net:4243\" ctop -a"
 
+alias dockersw2="DOCKER_HOST=dockersw2.vidal.net:2376 docker --tlsverify --tlscacert=/home/nkosinski/.dotfiles/docker/dockersw2/ca.pem --tlscert=/home/nkosinski/.dotfiles/docker/dockersw2/client-jenkins-cert.pem --tlskey=/home/nkosinski/.dotfiles/docker/dockersw2/client-jenkins-key.pem"
+
 alias mvn-deps-tree-vidal="mvn dependency:tree -Dincludes=\"com.vidal*:*:*:*\""
 alias mvn-version="xmlstarlet sel -N ns=\"http://maven.apache.org/POM/4.0.0\" -t -c \"/ns:project/ns:version/text()\" -n pom.xml"
 alias check-merges=~/.dotfiles/vidal/check-merges.sh
