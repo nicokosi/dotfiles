333e8ed7bb43d54fd1c995c55ac2a2027917d4f2
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2022-05-30 06:25:57 +0200

    🧹 Fix shellsheck warnings

--- system/disk-cleaning.zsh
+++ system/disk-cleaning.zsh
@@ -1,50 +1,52 @@
+#!/bin/zsh
+
 disk-cleanup() {
-  echo "Space before cleanup:"
+  print "Space before cleanup:"
   df -h | grep "/dev/disk1"
 
   ls ~/Library/Caches | xargs rm -rf
-  echo "App cache cleanup 🖥\n"
+  print "App cache cleanup 🖥\n"
 
   brew cleanup --prune=all
-  echo "Brew cleanup 🍻\n"
+  print "Brew cleanup 🍻\n"
 
   docker system prune --all --force
-  docker rmi --force $(docker images --all --quiet)
-  docker rm --force $(docker ps --all --quiet)
-  echo "Docker cleanup 🐳\n"
+  docker rmi --force "$(docker images --all --quiet)"
+  docker rm --force "$(docker ps --all --quiet)"
+  print "Docker cleanup 🐳\n"
 
   ls ~/.m2/repository | xargs rm -rf
-  cd ~/work/vidal
-  for project in `ls`
+  cd ~/work/vidal || exit
+  for project in $(ls)
   do
     if [[ -a $project/pom.xml ]]
     then
-      mvn-vidal -q -f $project/pom.xml clean
+      mvn-vidal -q -f "$project/pom.xml" clean
     fi
   done
-  echo "Maven cleanup 🪶\n"
+  print "Maven cleanup 🪶\n"
 
   ls ~/.gradle/caches | xargs rm -rf
-  for project in `ls`
+  for project in $(ls)
   do
     if [[ -a $project/build.gradle.kts ]]
     then
       ./gradlew clean
     fi
   done
-  echo "Gradle cleanup 🐘\n"
+  print "Gradle cleanup 🐘\n"
 
   ls ~/.ivy2/cache | xargs rm -rf
-  echo "Ivy cleanup 🐜\n"
+  print "Ivy cleanup 🐜\n"
 
   sdk flush archives && sdk flush broadcast && sdk flush temp
-  echo "SDKMAN! cleanup 🦸\n"
+  print "SDKMAN! cleanup 🦸\n"
 
 
   rm -r /Users/nicolas/work/vidal/api-installer/bin
   rm -r /Users/nicolas/work/vidal/perceval-installer/bin
-  echo "Misc file cleanup 🧹\n"
+  print "Misc file cleanup 🧹\n"
 
-  echo "Space after cleanup:"
+  print "Space after cleanup:"
   df -h | grep "/dev/disk1"
 }
