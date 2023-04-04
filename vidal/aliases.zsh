767673bbc4d2197b4902daf1800ee1c673bab610
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2023-04-04 14:14:57 +0200

    Fix Vidal dockersw2 alias 🐳

--- vidal/aliases.zsh
+++ vidal/aliases.zsh
@@ -4,7 +4,7 @@ alias dockerbo="DOCKER_HOST=\"tcp://dockerbo.vidal.net:4243\" docker"
 alias ctop-dockersw="DOCKER_HOST=\"tcp://dockersw.vidal.net:4243\" ctop -a"
 alias ctop-dockerbo="DOCKER_HOST=\"tcp://dockerbo.vidal.net:4243\" ctop -a"
 
-alias dockersw2="DOCKER_HOST=dockersw2.vidal.net:2376 docker --tlsverify --tlscacert=/home/nkosinski/.dotfiles/docker/dockersw2/ca.pem --tlscert=/home/nkosinski/.dotfiles/docker/dockersw2/client-jenkins-cert.pem --tlskey=/home/nkosinski/.dotfiles/docker/dockersw2/client-jenkins-key.pem"
+alias dockersw2='DOCKER_HOST="tcp://dockersw2.vidal.net:2376" docker --tlsverify --tlscacert=$HOME/.dotfiles/docker/dockersw2/ca.pem --tlscert=$HOME/.dotfiles/docker/dockersw2/cert.pem --tlskey=$HOME/.dotfiles/docker/dockersw2/key.pem'
 
 alias mvn-deps-tree-vidal="mvn dependency:tree -Dincludes=\"com.vidal*:*:*:*\""
 alias mvn-version="xmlstarlet sel -N ns=\"http://maven.apache.org/POM/4.0.0\" -t -c \"/ns:project/ns:version/text()\" -n pom.xml"
