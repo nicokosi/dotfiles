fdbbda354588cf6f16761c1e0872faabae3c454c
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2024-01-19 07:29:25 +0100

    Clean macports update script

--- macports/update.zsh
+++ macports/update.zsh
@@ -1,8 +1,6 @@
-echo-in() { #args= color, text
-  local NO_COLOR="\033[0m"
-  echo "${1}${2}${NO_COLOR}"
-}
-echo-in-blue() { echo-in "\033[0;34m" $1}
+#!/bin/zsh
+
+~/.dotfiles/system/echos.zsh
 
 macports-upgrades() {
   echo-in-blue "Upgrading MacPorts"
