f081f1df6f9ce4002cebea2b07835732398851f6
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2024-03-29 07:31:04 +0100

    Configre Chrome for npm tests

--- vidal/env.zsh
+++ vidal/env.zsh
@@ -2,4 +2,4 @@ export INSTALL4J_HOME_5_1_15="/opt/install4j5"
 export INSTALL4J_HOME_7_0_9="/opt/install4j7"
 
 # For npm front-end tests that require Chrome:
-export CHROME_BIN="/snap/bin/chromium"
+export CHROME_BIN="/opt/google/chrome/chrome"
