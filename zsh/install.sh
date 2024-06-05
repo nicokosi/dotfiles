206922a9960392d0a9cf97fc1e556535102f617c
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2024-06-05 06:21:27 +0200

    ✨ Search in shell history via up/down
    Via https://github.com/zsh-users/zsh-history-substring-search.

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,4 +6,6 @@ brew install zsh
 wget -P $TMPDIR https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/SourceCodePro%2BPowerline%2BAwesome%2BRegular.ttf
 mv $TMPDIR/SourceCodePro+Powerline+Awesome+Regular.ttf /Library/Fonts/
 
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
 exit 0
