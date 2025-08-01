4d48ba5479db082f40ce9dac10138bb9ffa62f24
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2025-08-01 08:59:54 +0200

    jenv/sdkman fixes

--- java/env.zsh
+++ java/env.zsh
@@ -1,7 +1,7 @@
 # jEnv configuration.
-export PATH="$HOME/.jenv/bin:$PATH"
-eval "$(jenv init -)"
+#export PATH="$HOME/.jenv/bin:$PATH"
+#eval "$(jenv init -)"
 
-export JAVA_HOME=$( jenv javahome )
+#export JAVA_HOME=$( jenv javahome )
 
-jenv enable-plugin maven > /dev/null 2>&1
+#jenv enable-plugin maven > /dev/null 2>&1
