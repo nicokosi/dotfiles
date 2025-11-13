014580e71cc2b6aa366fdb810e9af83b70ac8b54
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2025-11-13 05:53:21 +0100

    Add trash and npm installs in PATH

--- macports/env.zsh
+++ macports/env.zsh
@@ -2,3 +2,6 @@ alias port-export-installed-bundle='port installed requested > ~/.dotfiles/macpo
 alias port-import-installed-bundle='while read -r line; do
     port install "${line%% *}"
 done < ~/.dotfiles/macports/installed_ports.txt'
+
+export PATH="$HOME/.npm-global/bin:$PATH"
+export PATH="/usr/local/opt/trash/bin:$PATH"
