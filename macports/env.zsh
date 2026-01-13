c63a5d3e7ea1b1905f5a882f6d067337f41a63d7
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2026-01-13 06:08:40 +0100

    Fix macports export: remove header and versions

--- macports/env.zsh
+++ macports/env.zsh
@@ -1,4 +1,4 @@
-alias port-export-installed-bundle='port installed requested > ~/.dotfiles/macports/installed_ports.txt'
+alias port-export-installed-bundle='port echo requested > ~/.dotfiles/macports/installed_ports.txt'
 alias port-import-installed-bundle='while read -r line; do
     port install "${line%% *}"
 done < ~/.dotfiles/macports/installed_ports.txt'
