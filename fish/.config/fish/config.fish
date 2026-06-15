d4253c2922e9ebe0c5db0f2f3000e6a8a7fbabd7
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2026-06-15 17:45:28 +0200

    Install zoxide

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+# Configuration de Homebrew
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+zoxide init fish | source
