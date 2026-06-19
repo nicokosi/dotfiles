5a649dda31d72461d4954095f2459f81bc9febf8
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2026-06-19 07:59:36 +0200

    Add Git alias gf

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,13 +1,14 @@
 abbr -a g git
 abbr -a ga git add
 abbr -a gap git add -p
+abbr -a gc git commit
 abbr -a gco git checkout
 abbr -a gcp git cherry-pick
-abbr -a gc  git commit
-abbr -a gd  git diff
-abbr -a gl  git pull
+abbr -a gd git diff
+abbr -a gf git fetch
+abbr -a gl git pull
 abbr -a glo git log --oneline
-abbr -a gp  git push
+abbr -a gp git push
 abbr -a gpf git push --force-with-lease
 abbr -a gr git rebase
 abbr -a gst git status
