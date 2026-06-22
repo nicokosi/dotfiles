0658fbb07f5b7e2fbeaaffc7d90c33cbd048aeab
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2026-06-22 08:33:02 +0200

    Configure mcfly

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+    mcfly init fish | source
 end
-# Configuration de Homebrew
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-zoxide init fish | source
