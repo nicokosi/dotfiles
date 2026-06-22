0754964b32640dbf9958fae82087e95d1a1f2915
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2026-06-22 18:02:27 +0200

    Activate mise in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
     mcfly init fish | source
+    eval "$(mise activate fish)"
 end
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
