647b61373f86654a774ef4d023afbcf2e392d42b
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2026-07-17 18:18:06 +0200

    set AGENTSVIEW_PG_URL env for exploradora

--- fish/.config/fish/conf.d/vidal.fish
+++ fish/.config/fish/conf.d/vidal.fish
@@ -2,6 +2,7 @@
 set -gx INSTALL4J_HOME_5_1_15 "/opt/install4j5"
 set -gx INSTALL4J_HOME_7_0_9 "/opt/install4j7"
 set -gx OTEL_EXPORTER_OTLP_ENDPOINT "http://otel.dockersw.vidal.net"
+set -gx AGENTSVIEW_PG_URL "postgres://agentsview:agentsview@dockersw.vidal.net:5432/agentsview?sslmode=require"
 # For npm front-end tests that require Chrome:
 set -gx CHROME_BIN "/opt/google/chrome/chrome"
 
